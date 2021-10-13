46f884b8f83e0b2d536b1c7cf68bf56e81035d68
raghadhethnawi <raghadhethnawi@gmail.com>
2021-10-13 17:38:01 +0300

    'urls-and-link-building'

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from meetings.models import Meeting
 
 
 def welcome(request):
-    return HttpResponse("Welcome to the Meeting Planner!")
+    return render(request, "website/welcome.html",
+                  {"meetings": Meeting.objects.all()})
 
 
 def date(request):
